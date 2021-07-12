a7e05feb691c93c4a566d6ca801879efb1d779a5
B Shankar Upadhyay <upadhyaybs@live.com>
2021-07-12 18:02:22 -0500

    initial commit

---
+++ src/test/java/com/codify/api/gateway/SpringBootApiGatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codify.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiGatewayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
