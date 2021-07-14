cf8254be1b0684398e6a2913e745d7d5972f99ee
B Shankar Upadhyay <upadhyaybs@live.com>
2021-07-14 08:52:04 -0500

    new updates

--- src/main/java/com/codify/api/gateway/SpringBootApiGatewayDemoApplication.java
+++ src/main/java/com/codify/api/gateway/SpringBootApiGatewayDemoApplication.java
@@ -2,14 +2,10 @@ package com.codify.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-//@EnableZuulProxy
+
 @EnableEurekaClient
-//@EnableHystrix
 @SpringBootApplication
 public class SpringBootApiGatewayDemoApplication {
 
